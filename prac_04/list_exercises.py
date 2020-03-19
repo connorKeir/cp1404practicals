77e6da0dedd3ae4807069e5317abd52985c1b79f
connorKeir <connor.keir@my.jcu.edu.au>
2020-03-19 16:53:37 +1000

    Complete the Woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,6 +1,11 @@
 def main():
-    count = 0
-    numbers = []
+    count = 0  # initialise count at 0
+    numbers = []  # initialise numbers list
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']  # initialise approved users list
+
+    # basic list operations activities
     while count < 5:  # ask for 5 numbers
         number = int(input("Enter a number: "))  # add user input to number variable
         numbers.append(number)  # add number from user to list
@@ -11,10 +16,24 @@ def main():
     print("The largest number is {}".format(max(numbers)))
     print("The average of the numbers is {}".format(average_calculator_list(numbers)))
 
+    print("~~~~~~~~~~~~~")  # create a gap in between exercises
+
+    # Woefully inadequate security checker
+    username = input("Enter your username: ")
+    security_checker(username, usernames)
+
+
 
 def average_calculator_list(list):
     # calculate the average number in a list of integers
     return sum(list) / len(list)
+
+def security_checker(user, usernames):
+    # check if the user is an approved user
+    if user in usernames:
+        print("Access Granted")
+    else:
+        print("Access denied")
 
 
 main()
