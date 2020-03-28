eaff8bfcf0af5e667cf5f3beca69c48adce2f002
connorKeir <connor.keir@my.jcu.edu.au>
2020-03-28 14:25:56 +1000

    add constants

--- prac_01/electricity_bill.py
+++ prac_01/electricity_bill.py
@@ -1,11 +1,28 @@
-print("Electricity bill estimator")
+TARIFF_11 = 0.244618
+TARIFF_31 = 0.136928
 
 
-# collect base user inputs for estimation
-price_per_kWh = float(input("Enter cents per kWh: "))
-daily_use_kwh = float(input("Enter daily use in kWh: "))
-days_in_billing = float(input("Enter number of billing days: "))
+def main():
+    print("Electricity bill estimator 2.0")
 
-bill_total = ((price_per_kWh/100)*daily_use_kwh)*days_in_billing  # equation for bill total
+    # collect base user inputs for estimation
+    # price_per_kWh = float(input("Enter cents per kWh: "))  # removed when updated to 2.0
+    tariff = float(input("Which tariff? 11 or 31: "))
+    if tariff == 11:
+        tariff = float(TARIFF_11)
+    elif tariff == 31:
+        tariff = float(TARIFF_31)
 
-print("Estimated bill: ${:.2f}".format(bill_total))  # print bill total
+    daily_use_kwh = float(input("Enter daily use in kWh: "))
+    days_in_billing = float(input("Enter number of billing days: "))
+    print(tariff)
+    bill_total = bill_calculator(tariff, daily_use_kwh, days_in_billing)
+
+    print("Estimated bill: ${:.2f}".format(bill_total))  # print bill total
+
+
+def bill_calculator(cost, daily_use, period):
+    return (cost * daily_use) * period  # equation bill estimate
+
+
+main()
