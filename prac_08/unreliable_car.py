df9bb326c67c90037d365e2f109a734547279c5f
connorKeir <connor.keir@my.jcu.edu.au>
2020-05-06 17:48:40 +1000

    Add and test SilverServiceTaxi class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,7 +7,7 @@ from random import randrange
 
 
 class UnreliableCar(Car):
-    def __init__(self, name, fuel, reliability):
+    def __init__(self, name='Unreliable Car', fuel=0, reliability=50):
         """Initialise a UnreliableCar instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
