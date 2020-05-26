e282b0d58b09ab6ed0795f368379d1090ded53c8
connorKeir <connor.keir@my.jcu.edu.au>
2020-05-26 15:20:15 +1000

    fix the repeat string function

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,10 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    repeated_string_list = [ ]
+    for i in range(n):
+        repeated_string_list.append(s)
+    return ' '.join(repeated_string_list)
 
 
 def is_long_word(word, length=5):
@@ -32,7 +35,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+    # fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
